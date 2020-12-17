4bc5701dc8736031719414a4625c06cf54068ed2
Alejandro Alvarez <aalvarezmartin@gmail.com>
2020-12-17 11:08:31 +0100

    Adding queryparm to search users

--- src/main/java/com/aziru/restworld/controllers/UserController.java
+++ src/main/java/com/aziru/restworld/controllers/UserController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aziru.restworld.models.User;
@@ -23,17 +26,30 @@ public class UserController {
 	private UserService userService;
 
 	@GetMapping // Method HTTP + resource: handler method
-	public ResponseEntity<List<User>> getUsers() {
-		return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+	public ResponseEntity<List<User>> getUsers(
+			@RequestParam(value = "startWith", required = false) final String startWith) {
+		return new ResponseEntity<>(userService.getUsers(startWith), HttpStatus.OK);
 	}
 
-	@RequestMapping("/{userName}")
-	public ResponseEntity<User> getUserByUserName(@PathVariable("userName") String userName) {
-		return new ResponseEntity<>(userService.getUserByUserName(userName), HttpStatus.OK);
+	@RequestMapping("/{username}")
+	public ResponseEntity<User> getUserByUserName(@PathVariable("username") final String username) {
+		return new ResponseEntity<>(userService.getUserByUserName(username), HttpStatus.OK);
 	}
 
 	@PostMapping
-	public ResponseEntity<User> addUser(@RequestBody User user) {
+	public ResponseEntity<User> addUser(@RequestBody final User user) {
 		return new ResponseEntity<>(userService.createUser(user), HttpStatus.OK);
+	}
+
+	@PutMapping(value = "/{username}")
+	public ResponseEntity<User> updateUser(@PathVariable("username") final String username,
+			@RequestBody final User user) {
+		return new ResponseEntity<>(userService.updateUser(username, user), HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "/{username}")
+	public ResponseEntity<Void> deleteUser(@PathVariable("username") final String username) {
+		userService.deleteUser(username);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
--- src/main/java/com/aziru/restworld/service/UserService.java
+++ src/main/java/com/aziru/restworld/service/UserService.java
@@ -2,9 +2,11 @@ package com.aziru.restworld.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -19,11 +21,11 @@ public class UserService {
 	@Autowired
 	private Faker faker;
 
-	private List<User> users = new ArrayList<>();
+	private final List<User> users = new ArrayList<>();
 
 	@PostConstruct
 	public void init() {
-		for (int i = 0; i < 100; i++) {
+		for (var i = 0; i < 100; i++) {
 			users.add(new User(faker.funnyName().name(), faker.name().username(), faker.dragonBall().character()));
 		}
 	}
@@ -31,13 +33,17 @@ public class UserService {
 	/**
 	 * @return the users
 	 */
-	public List<User> getUsers() {
-		return users;
+	public List<User> getUsers(final String startWith) {
+		if (StringUtils.isNoneEmpty(startWith)) {
+			return users.stream().filter(u -> u.getUserName().startsWith(startWith)).collect(Collectors.toList());
+		} else {
+			return users;
+		}
 	}
 
 	/**
 	 * Get user by user name
-	 * 
+	 *
 	 * @param username
 	 * @return
 	 */
@@ -49,16 +55,39 @@ public class UserService {
 
 	/**
 	 * Create a new user
-	 * 
+	 *
 	 * @param user
 	 * @return
-	 */	
+	 */
 	public User createUser(final User user) {
 		if (users.stream().anyMatch(u -> u.getUserName().equals(user.getUserName()))) {
 			throw new ResponseStatusException(HttpStatus.CONFLICT,
 					String.format("El usuario %s ya existe", user.getUserName()));
 		}
 		users.add(user);
-		return user;		
+		return user;
+	}
+
+	/**
+	 * Update a existing user
+	 *
+	 * @param user
+	 * @return
+	 */
+	public User updateUser(final String username, final User user) {
+		final var userToBeUpdate = getUserByUserName(username);
+		userToBeUpdate.setUserName(user.getUserName());
+		userToBeUpdate.setPassword(user.getPassword());
+		return userToBeUpdate;
+
+	}
+
+	/**
+	 * Delete a existing user
+	 *
+	 * @param user
+	 */
+	public void deleteUser(final String username) {
+		users.remove(getUserByUserName(username));
 	}
 }
